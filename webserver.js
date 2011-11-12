c0618992b8102373fbffae7813ff5cd187a7bef4
Telmo Costa <telmo.costa@yunit.pt>
2011-11-12 10:20:56 +0000

    wrap up (again)

--- webserver.js
+++ webserver.js
@@ -1,11 +1,24 @@
 #!/usr/bin/env node
 
 var Webserver = require('hook.io-webserver').Webserver;
+var Hook = require('hook.io').Hook;
+
+function Hooker() {
+  Hook.call(this);
+}
+require('util').inherits(Hooker, Hook);
+var hooker = new Hooker({});
 
 var webserver = new Webserver({
   name: 'hook.io-webserver',
   port: 9000,
-  webroot: './public'
+  webroot: __dirname + '/public'
 });
 
 webserver.start();
+
+hooker.on('serial::knobs', function(data) {
+  webserver.sendToBrowser('serial::knobs', data);
+});
+
+hooker.start();
